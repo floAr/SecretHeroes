e8631c36e3387f7a1f99e07666de330de0aa018a
Florian Uhde <floar@mx.de>
2021-03-21 14:56:27 +0100

    update with arena

--- Build/_build_default.framework.js
+++ Build/_build_default.framework.js
@@ -1298,7 +1298,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2621248;
+STATICTOP = STATIC_BASE + 2622032;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3284,7 +3284,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2621248;
+var STATIC_BUMP = 2622032;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
