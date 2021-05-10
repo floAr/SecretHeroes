d778f2e0bb09840461111554bb1fe33a1612d9a4
floAr <floar@gmx.de>
2021-05-10 19:51:53 +0200

    adding the interface for the leaderboard props

---
+++ Webapp/src/components/Leaderboard.tsx
@@ -0,0 +1,17 @@
+
+export interface PlayerStats {
+
+  address: string, // player address
+  battles: number, // number of battles
+  losses: number, // number of losses
+  score: number, // players score
+  third_in_two_way_ties: number, // numbers of taking 3rd place in a tie
+  ties: number, // number ties
+  wins: number //number of wins
+}
+export interface LeaderboardProps {
+  all_time: PlayerStats,
+  tournament: PlayerStats,
+  tournament_started: number // "seconds after 01/01/1970 in which the tournament started"
+}
+
