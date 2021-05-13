d5a7096c4bed85eb8a49b6dcc01b156a17a1c2e1
Florian Uhde <floar@mx.de>
2021-05-13 09:42:14 +0200

    add viewingkey context

---
+++ Webapp/src/secret/ViewingKeysContext.tsx
@@ -0,0 +1,49 @@
+import React, { createContext, useCallback, useEffect, useState } from 'react'
+
+export type viewingKeyPair = {
+  key: string
+  address: string
+}
+
+const storageKey = 'viewingKeys'
+
+export interface ViewingKeyContext {
+  getViewingKey: (address: string) => string | undefined
+  addViewingKey: (keyPair: viewingKeyPair) => void
+}
+
+export const viewingKeyContext = createContext<ViewingKeyContext>({
+  getViewingKey: (_: string) => undefined,
+  addViewingKey: (_: viewingKeyPair) => {
+    // do nothing
+  }
+})
+
+const ViewingKeyContextProvider: React.FC = ({ children }) => {
+  const isBrowser = typeof window !== 'undefined'
+  const [viewingKey, setViewingKey] = useState<viewingKeyPair[]>(isBrowser ? JSON.parse(localStorage.getItem(storageKey) ?? '[]') : [])
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(viewingKey))
+  }, [viewingKey])
+
+  const getViewingKey = useCallback(
+    (address: string) => {
+      const item = viewingKey.find(vk => vk.address === address)
+      if (item) return item.key
+      return undefined
+    },
+    [viewingKey]
+  )
+
+  const addViewingKey = useCallback(
+    (vkp: viewingKeyPair) => {
+      const other = viewingKey.filter(vk => vk.address !== vkp.address)
+      setViewingKey([...other, vkp])
+    },
+    [viewingKey]
+  )
+
+  return <viewingKeyContext.Provider value={{ getViewingKey, addViewingKey }}>{children}</viewingKeyContext.Provider>
+}
+
+export default ViewingKeyContextProvider
