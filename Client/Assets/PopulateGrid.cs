cd6cf34ca8103ea80f8b21cfc12a37a52adec88c
AaronRadina <kaizencodesix@gmail.com>
2021-09-16 01:08:20 -0700

    GridIcons
    Added gridview icons creator and layout manager to spawn prefabs... also new.

---
+++ Client/Assets/PopulateGrid.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateGrid : MonoBehaviour
+{
+    public GameObject prefab;
+    public int numberToCreate;
+    void Start()
+    {
+        Populate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Populate()
+    {
+        GameObject newObj;
+
+        for (int i = 0; i < numberToCreate; i++)
+        {
+            newObj = (GameObject)Instantiate(prefab, transform);
+           // newObj.GetComponent<Image>().color = Random.ColorHSV();
+
+        }
+    }
+}
