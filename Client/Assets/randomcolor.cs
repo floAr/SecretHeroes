ef94872cdeb3aeaef649b9f6ac71caedeec96532
AaronRadina <kaizencodesix@gmail.com>
2021-10-04 17:58:09 -0700

    PanelCreation
    ScalingPanels and Creating Upgrades Panel

---
+++ Client/Assets/randomcolor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomcolor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Pick a random, saturated and not-too-dark color
+        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
